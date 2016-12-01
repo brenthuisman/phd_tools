669eb52ec05852d10c982e707f4f9e30d363c09b
Brent Huisman <mail@brenthuisman.net>
2016-12-01 16:50:58 +0100

    range plots are good

--- analysis.spot.range.py
+++ analysis.spot.range.py
@@ -2,11 +2,9 @@
 import dump, numpy as np,plot,sys,glob2 as glob,os
 from scipy.stats import chisquare,norm
 
-clrs=plot.colors
-np.random.seed(65983247)
+#np.random.seed(65983247)
 
-rootn='ipnl-patient-spot-auger.root'
-#rootn='iba-patient-spot-auger.root'
+rootn='-patient-spot-auger.root'
 
 def getctset(nprim,ct1,ct2,name):
 	ctset6 = {'ct':{},'rpct':{},'name':name,'nprim':nprim}
@@ -19,93 +17,146 @@ def getctset(nprim,ct1,ct2,name):
 	ctset6['ct']['x'] = []
 	ctset6['rpct']['x'] = []
 	
-	ctset6['ct']['files'] = glob.glob(ct1+"/**/"+rootn)
-	ctset6['rpct']['files'] = glob.glob(ct2+"/**/"+rootn)
+	ctset6['ct']['files'] = glob.glob(ct1+"/**/"+name+rootn)
+	ctset6['rpct']['files'] = glob.glob(ct2+"/**/"+name+rootn)
 
 	for ffilen in ctset6['ct']['files']:
 		print 'opening',ffilen
-		for key,val in dump.thist2np_xy(ffilen).items():
-			if key == 'reconstructedProfileHisto':
-				ctset6['ct']['x'] = val[0] #no need to append, is same for all
-				ctset6['ct']['data'].append(plot.ipnlnoise(val[1],nprim))
-				#ctset6['ct']['data'].append(plot.ibanoise(val[1],nprim))
-		ctset6['ct']['falloff'].append(plot.ipnl_fit_range(ctset6['ct']['x'],ctset6['ct']['data'][-1]))
+		try:
+			for key,val in dump.thist2np_xy(ffilen).items():
+				if key == 'reconstructedProfileHisto':
+					ctset6['ct']['x'] = val[0] #no need to append, is same for all
+					if 'ipnl' == name:
+						ctset6['ct']['data'].append(plot.ipnlnoise(val[1],nprim))
+					elif 'iba' == name: #reverse
+						ctset6['ct']['data'].append(plot.ipnlnoise(val[1][::-1],nprim))
+			ctset6['ct']['falloff'].append(plot.ipnl_fit_range(ctset6['ct']['x'],ctset6['ct']['data'][-1]))
+		except IndexError:
+			print "Empty file detected, skipping"
 	for ffilen in ctset6['rpct']['files']:
 		print 'opening',ffilen
-		for key,val in dump.thist2np_xy(ffilen).items():
-			if key == 'reconstructedProfileHisto':
-				ctset6['rpct']['x'] = val[0] #no need to append, is same for all
-				ctset6['rpct']['data'].append(plot.ipnlnoise(val[1],nprim))
-				#ctset6['rpct']['data'].append(plot.ibanoise(val[1],nprim))
-		ctset6['rpct']['falloff'].append(plot.ipnl_fit_range(ctset6['rpct']['x'],ctset6['rpct']['data'][-1]))
+		try:
+			for key,val in dump.thist2np_xy(ffilen).items():
+				if key == 'reconstructedProfileHisto':
+					ctset6['rpct']['x'] = val[0] #no need to append, is same for all
+					if 'ipnl' == name:
+						ctset6['rpct']['data'].append(plot.ipnlnoise(val[1],nprim))
+					elif 'iba' == name: #reverse
+						ctset6['rpct']['data'].append(plot.ipnlnoise(val[1][::-1],nprim))
+			ctset6['rpct']['falloff'].append(plot.ipnl_fit_range(ctset6['rpct']['x'],ctset6['rpct']['data'][-1]))
+		except IndexError:
+			print "Empty file detected, skipping"
 	
 	return ctset6
 
 def plotrange(ax1,ct):
-	
 	for i in range(len(ct['ct']['data'])):
-		x,y,fo=ct['ct']['x'],ct['ct']['data'][i],ct['ct']['falloff'][i][0]
-		ax1.step(x, y, label=ct['name']+'ct', color='steelblue', lw=1, where='mid',clip_on=False)
-		ax1.axvline(fo, color='#999999', ls='--')
+		x,y,fo=ct['ct']['x'],ct['ct']['data'][i],ct['ct']['falloff'][i]
+		ax1.step(x, y, label=ct['name']+'ct', color='indianred', lw=1, where='mid',clip_on=False, alpha=0.5)
+		ax1.axvline(fo, color='indianred', ls='-',lw=1, alpha=0.5)
 	
 	ax1.set_xlabel('Depth [mm]', fontsize=8)
 	ax1.set_ylabel('PG detected [counts]', fontsize=8)
-	#ax1.set_ylim(bottom=0,top=ymax)
 	ax1.set_title(plot.sn(ct['nprim'],0)+' primaries', fontsize=8)
 	plot.texax(ax1)
 
 	for i in range(len(ct['rpct']['data'])):
-		x,y,fo=ct['rpct']['x'],ct['rpct']['data'][i],ct['rpct']['falloff'][i][0]
-		ax1.step(x, y, label=ct['name']+'ct', color='steelblue', lw=1, where='mid',clip_on=False)
-		ax1.axvline(fo, color='#999999', ls='--')
+		x,y,fo=ct['rpct']['x'],ct['rpct']['data'][i],ct['rpct']['falloff'][i]
+		ax1.step(x, y, label=ct['name']+'ct', color='steelblue', lw=1, where='mid',clip_on=False, alpha=0.5)
+		ax1.axvline(fo, color='steelblue', ls='-',lw=1, alpha=0.5)
 	
 	ax1.set_title('PG detection for '+plot.sn_mag(ct['nprim'])+'primaries', fontsize=8)
+	ax1.set_ylim(bottom=0)
 	plot.texax(ax1)
 
-color_index=0
-def plotfodist(ax1,ct,zs):
-	global color_index
+
+def plotfodiffdist(ax1,ct,zs):
 	fodiff=[]
 	for i in range(len(ct['ct']['data'])):
-		foct=ct['ct']['falloff'][i][0]
+		foct=ct['ct']['falloff'][i]
 		for i in range(len(ct['rpct']['data'])):
-			forpct=ct['rpct']['falloff'][i][0]
+			forpct=ct['rpct']['falloff'][i]
 			fodiff.append(forpct-foct)
-	#ax1.hist(fodiff, 20, facecolor='steelblue', alpha=0.75)
-	#ax1.hist(fodiff, 20, facecolor=plot.colors[color_index], alpha=0.75,label=plot.sn_mag(ct['nprim'])+' primaries')
-	y,bins=np.histogram(fodiff, np.arange(-120,120,10))
+	y,bins=np.histogram(fodiff, np.arange(-120,120,1))
+	(mu, sigma) = norm.fit(fodiff)
+	#print mu, sigma
 	bincenters = 0.5*(bins[1:]+bins[:-1])
-	c = [plot.colors[color_index]]*len(y)
-	ax1.bar(bincenters,y, color=c, zs=zs, zdir='y', alpha=0.75,label=plot.sn_mag(ct['nprim'])+' primaries')
-	color_index+=1
+	c = ['grey']*len(y)
+	ax1.bar(bincenters,y, color=c,lw=0.2, zs=zs, zdir='y',label=plot.sn_mag(ct['nprim'])+' primaries')
+	
+	ax1.set_xlim3d(-10,20)
 	
 	ax1.set_xlabel('Detected Shifts [mm]', fontsize=8)
 	ax1.set_zlabel('[counts]', fontsize=8)
-	#ax1.set_ylim(bottom=0,top=ymax)
+	ax1.locator_params(axis='y',nbins=4)
+	labels = ["$10^9$","$10^8$","$10^7$","$10^6$"]
+	ax1.set_yticklabels(labels, fontsize=8, va='baseline', ha='left')
+	ax1.set_ylabel('Primaries [nr]', fontsize=8)
+	#zs/10.
+	plt.figtext(0.05, 0.9-zs/400.,plot.sn_mag(ct['nprim'])+": $\mu$ = "+str(mu)[:3]+", $\sigma$ = "+str(sigma)[:3], ha='left', va='center', fontsize=6, transform=ax1.transAxes)
+
+
+def plotfodist(ax1,ct,zs):
+	fo_ct=[]
+	fo_rpct=[]
 	
-	ax1.set_title('PG detection shifts', fontsize=8)
-	#plot.texax(ax1)
+	for i in range(len(ct['ct']['data'])):
+		x,y,fo=ct['ct']['x'],ct['ct']['data'][i],ct['ct']['falloff'][i]
+		fo_ct.append(fo)
+	
+	for i in range(len(ct['rpct']['data'])):
+		x,y,fo=ct['rpct']['x'],ct['rpct']['data'][i],ct['rpct']['falloff'][i]
+		fo_rpct.append(fo)
+	
+	y,bins=np.histogram(fo_ct, np.arange(-120,120,1))
+	bincenters = 0.5*(bins[1:]+bins[:-1])
+	c = ['indianred']*len(y)
+	ax1.bar(bincenters,y, color=c,lw=0.2, zs=zs, zdir='y', alpha=0.5, label=plot.sn_mag(ct['nprim'])+' primaries')
+	
+	y,bins=np.histogram(fo_rpct, np.arange(-120,120,1))
+	bincenters = 0.5*(bins[1:]+bins[:-1])
+	c = ['steelblue']*len(y)
+	ax1.bar(bincenters,y, color=c,lw=0.2, zs=zs, zdir='y', alpha=0.5, label=plot.sn_mag(ct['nprim'])+' primaries')
+	
+	ax1.set_xlim3d(-10,20)
+	
+	ax1.set_xlabel('Falloff Position [mm]', fontsize=8)
+	ax1.set_zlabel('[counts]', fontsize=8)
+	ax1.locator_params(axis='y',nbins=4)
+	labels = ["$10^9$","$10^8$","$10^7$","$10^6$"]
+	ax1.set_yticklabels(labels, fontsize=8, va='baseline', ha='left')
+	ax1.set_ylabel('Primaries [nr]', fontsize=8)
 
-###################################
+#########################################################################################################
 
-ctset_int_6 = getctset(1e6,'fromcluster/nprims/run.yjzw','fromcluster/nprims/run.wfmx','')
-ctset_int_7 = getctset(1e7,'fromcluster/nprims/run.41VE','fromcluster/nprims/run.NE7B','')
-ctset_int_8 = getctset(1e8,'fromcluster/nprims/run.bfrX','fromcluster/nprims/run.rOx1','')
-ctset_int_9 = getctset(1e9,'fromcluster/nprims/run.xTcG','fromcluster/nprims/run.b2Aj','')
+typ='ipnl'
+#typ='iba'
+ctset_int_6 = getctset(1e6,'fromcluster/nprims/run.yjzw','fromcluster/nprims/run.wfmx',typ)
+ctset_int_7 = getctset(1e7,'fromcluster/nprims/run.41VE','fromcluster/nprims/run.NE7B',typ)
+ctset_int_8 = getctset(1e8,'fromcluster/nprims/run.bfrX','fromcluster/nprims/run.rOx1',typ)
+ctset_int_9 = getctset(1e9,'fromcluster/nprims/run.xTcG','fromcluster/nprims/run.b2Aj',typ)
 
-#f, ((ax1,ax2),(ax3,ax4)) = plot.subplots(nrows=2, ncols=2, sharex=False, sharey=False)
+#typ='iba'
+#iba_ctset_int_6 = getctset(1e6,'fromcluster/nprims/run.yjzw','fromcluster/nprims/run.wfmx',typ)
+#iba_ctset_int_7 = getctset(1e7,'fromcluster/nprims/run.41VE','fromcluster/nprims/run.NE7B',typ)
+#iba_ctset_int_8 = getctset(1e8,'fromcluster/nprims/run.bfrX','fromcluster/nprims/run.rOx1',typ)
+#iba_ctset_int_9 = getctset(1e9,'fromcluster/nprims/run.xTcG','fromcluster/nprims/run.b2Aj',typ)
 
-#plotrange(ax1,ctset_int_9)
-#plotrange(ax2,ctset_int_8)
-#plotrange(ax3,ctset_int_7)
-#plotrange(ax4,ctset_int_6)
-#f.subplots_adjust(hspace=.5)
-#ax1.set_xlabel('')
-#ax2.set_xlabel('')
-#ax2.set_ylabel('')
-#ax4.set_ylabel('')
-#f.savefig(rootn.split('-')[0]+'-spot-falloff.pdf', bbox_inches='tight')
-#plot.close('all')
+#########################################################################################################
+
+f, ((ax1,ax2),(ax3,ax4)) = plot.subplots(nrows=2, ncols=2, sharex=False, sharey=False)
+
+plotrange(ax1,ctset_int_9)
+plotrange(ax2,ctset_int_8)
+plotrange(ax3,ctset_int_7)
+plotrange(ax4,ctset_int_6)
+f.subplots_adjust(hspace=.5)
+ax1.set_xlabel('')
+ax2.set_xlabel('')
+ax2.set_ylabel('')
+ax4.set_ylabel('')
+f.savefig(typ+'-spot-falloff.pdf', bbox_inches='tight')
+plot.close('all')
 
 #########################################################################################################
 
@@ -113,14 +164,30 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
 fig = plt.figure()
-ax1 = fig.add_subplot(111, projection='3d')
+ax1 = plt.axes(projection='3d')
+ax1.view_init(30, -50)
 
-#f, ax1 = plt.subplots(nrows=1, ncols=1, sharex=False, sharey=False, projection='3d')
+plotfodiffdist(ax1,ctset_int_9,0)
+plotfodiffdist(ax1,ctset_int_8,10)
+plotfodiffdist(ax1,ctset_int_7,20)
+plotfodiffdist(ax1,ctset_int_6,30)
+#plt.tight_layout(rect = [-0.1, 0.0, 1.0, 1.1])#L,B,R,T
+fig.savefig(typ+'-spot-falloffdiffdist.pdf')#, bbox_inches='tight')
+plt.close('all')
+
+#########################################################################################################
+
+fig = plt.figure()
+ax1 = plt.axes(projection='3d')
+ax1.view_init(30, -50)
 
 plotfodist(ax1,ctset_int_9,0)
 plotfodist(ax1,ctset_int_8,10)
 plotfodist(ax1,ctset_int_7,20)
 plotfodist(ax1,ctset_int_6,30)
-#ax1.legend(loc='upper right',frameon=False)#, bbox_to_anchor=(1.2, 1.))
-fig.savefig(rootn.split('-')[0]+'-spot-falloffdist.pdf', bbox_inches='tight')
-plot.close('all')
+
+#plt.legend()#shadow = True,frameon = True,fancybox = True,ncol = 1,fontsize = 'x-small',loc = 'lower right')
+
+#plt.tight_layout(rect = [-0.1, 0.0, 1.0, 1.1])#L,B,R,T
+plt.savefig(typ+'-spot-falloffdist.pdf')#, bbox_inches='tight')
+plt.close('all')
--- plot/__init__.py
+++ plot/__init__.py
@@ -1,7 +1,10 @@
 from texify import *
 import numpy as np,matplotlib.pyplot as plt
 from math import floor, log10
-from scipy import interpolate
+import scipy.interpolate
+import scipy.signal
+import scipy.optimize
+import plot
 
 #colors = "rgbmyck"*20
 colors = ['#3778bf','#feb308','#7b0323','#7bb274','#a8a495','#825f87']*20
@@ -159,7 +162,7 @@ def getloghist(data,nrbin=50):
 	return centers,hist
 
 
-def ipnl_fit_range(x,y):
+def ipnl_fit_range_oud(x,y):
 	#take max
 	y=np.array(y)
 	#maxind = np.argmax(y)
@@ -176,8 +179,8 @@ def ipnl_fit_range(x,y):
 	xbp=x[hm_left:hm_right]
 	ybp=y[hm_left:hm_right]
 	#print maxind,xbp,ybp,y
-	tck,u = interpolate.splprep( [xbp,ybp] ,k=3 )#cubic spline
-	xnew,ynew = interpolate.splev(  np.linspace( 0, 1, 200 ), tck,der = 0)
+	tck,u = scipy.interpolate.splprep( [xbp,ybp] ,k=3 )#cubic spline
+	xnew,ynew = scipy.interpolate.splev(  np.linspace( 0, 1, 200 ), tck,der = 0)
 	#take mean value in tail-window as minimum
 	floor=np.mean(y[hm_right:])
 	#take max val in BP window as max
@@ -189,6 +192,133 @@ def ipnl_fit_range(x,y):
 	falloffpos = xnew[nextind]
 	#ax1.plot( xbp,ybp,'' , xnew ,ynew )
 	return falloffpos,[xbp,ybp,'',xnew,ynew]
+
+
+def ipnl_fit_range(x,y):
+	y=np.array(y)
+	
+	#f, ax1 = plot.subplots(nrows=1, ncols=1, sharex=False, sharey=False)
+	#ax1.step( x,y, color='moccasin') #bindata
+	
+	#smooth that shit out.
+	#https://en.wikipedia.org/wiki/Smoothing_spline
+	y2 = scipy.signal.cspline1d(y,lamb=2)
+	y_intpol_f = scipy.interpolate.interp1d(x,y2,kind='cubic') #interpolate function
+	x_intpol = np.linspace(x[0],x[-1],1024,endpoint=False)
+	y_intpol = y_intpol_f(x_intpol) #interpolate for x_intpol
+	y_intpol = y_intpol.clip(min=0) #set any possible negatives to zero
+	#ax1.step(x,y2,color='steelblue')
+	#ax1.plot(x_intpol,y_intpol,color='indianred')
+	
+	#take max
+	maxind = np.where(y_intpol == y_intpol.max())[0][-1] #so that we find the LAST index where argmax(y)
+	#ax1.axvline(x_intpol[maxind],color='green')
+	
+	#get 25 percentile, and take mean as baseline.
+	baseline = y_intpol[y_intpol < np.percentile(y_intpol, 25)].mean()
+	#ax1.axhline(baseline,color='green')
+	#ax1.axhline(y_intpol.max(),color='green')
+	
+	falloff = y_intpol[maxind]-baseline
+	falloff_index=len(x_intpol)-1
+	while y_intpol[falloff_index] < 0.5*falloff+baseline:
+		falloff_index-=1
+	falloff_pos=x_intpol[falloff_index]
+	#ax1.axvline(falloff_pos,color='green')
+	
+	#f.savefig('recons.pdf', bbox_inches='tight')
+	#plot.close('all')
+	#quit()
+	return falloff_pos
+
+
+def gueth_fit_range(xs,profile,falloff=.5,window_width=80.,smooth=2):
+    # x,y=xs,profile
+    xs=np.array(xs)
+    profile=np.array(profile)/sum(profile)
+    assert(xs.shape==profile.shape)
+    
+    profile_falloff = profile.mean()*1.3
+    index_max = profile.size-1
+    assert(profile[index_max]<profile_falloff)
+    while profile[index_max]<profile_falloff:
+        index_max -= 1
+
+    x_max = xs[index_max]
+    x_res = xs[index_max+1]-xs[index_max]
+
+    index_window = int(window_width/x_res)
+    xs_window = xs[index_max-index_window:index_max+index_window]
+    profile_window = profile[index_max-index_window:index_max+index_window]
+    profile_window_spline = scipy.signal.cspline1d(profile_window,lamb=smooth)
+
+    f, ax1 = plot.subplots(nrows=1, ncols=1, sharex=False, sharey=False)
+    ax1.plot(xs,profile)
+    ax1.plot(xs_window,profile_window,'o-')
+    xs_window_interp = np.linspace(xs_window.min(),xs_window.max(),128,endpoint=False)
+    profile_window_interp = scipy.signal.cspline1d_eval(profile_window_spline,xs_window_interp,dx=x_res,x0=xs_window_interp.min())
+    ax1.plot(xs_window_interp,profile_window_interp,'*-')
+    #f.savefig('testplot-falloff.pdf', bbox_inches='tight')
+    #plot.close('all')
+    #quit()
+    
+    def eval_profile_window_spline(position):
+        #assert(type(position)==float64)
+        assert(position>=xs_window.min() and position<=xs_window.max())
+        value = scipy.signal.cspline1d_eval(profile_window_spline,[position],dx=x_res,x0=xs_window.min())
+        return value[0]
+
+    def maximum_profile_window_spline(position):
+        #position = position[0]
+        value = eval_profile_window_spline(position) 
+        return -value
+
+    x_max_spline = scipy.optimize.fminbound(maximum_profile_window_spline,xs_window.min(),xs_window.max(),disp=False)
+    #x_max_spline = x_max_spline[0]
+    profile_max_spline = eval_profile_window_spline(x_max_spline)
+    
+    ax1.axvline(x_max_spline)
+    ax1.axhline(profile_max_spline)
+
+    #print "x_max_spline",x_max_spline
+    #print "profile_max_spline",profile_max_spline
+
+    def dichotomy_profile_window_spline(bounds,bounds_level,level):
+        assert(bounds_level[0]<level and bounds_level[1]>level)
+
+        # if level diff become too small then linear intepolation for final step
+        if (bounds_level[1]-bounds_level[0]) < 2e-3:
+            alpha = (level-bounds_level[0])/(bounds_level[1]-bounds_level[0])
+            assert(alpha>0 and alpha<1)
+            return alpha*(bounds[1]-bounds[0])+bounds[0]
+
+        middle = bounds[0]+bounds[1]
+        middle /= 2.
+        middle_level = eval_profile_window_spline(middle)
+
+        if middle_level > level:
+            return dichotomy_profile_window_spline((bounds[0],middle),(bounds_level[0],middle_level),level)
+        else:
+            return dichotomy_profile_window_spline((middle,bounds[1]),(middle_level,bounds_level[1]),level)
+
+    def find_level_profile_window_spline(level,bounds):
+        return dichotomy_profile_window_spline(bounds,(eval_profile_window_spline(bounds[0]),eval_profile_window_spline(bounds[1])),level)
+
+    profile_falloff_spline = profile_max_spline * falloff
+    ax1.axhline(profile_falloff_spline)
+    x_falloff_spline = find_level_profile_window_spline(profile_falloff_spline,(xs_window.max(),x_max_spline))
+
+    ax1.axvline(x_max,color='indianred')
+    ax1.axvline(x_max_spline,color='steelblue')
+    ax1.axhline(profile_max_spline)
+    ax1.axvline(x_falloff_spline,color='mediumseagreen')
+    ax1.axhline(profile_falloff_spline)
+    #ax1.xlim(x_max-1.5*window_width,x_max+1.5*window_width)
+    f.savefig('testplot-falloff.pdf', bbox_inches='tight')
+    plot.close('all')
+    quit()
+
+    return x_falloff_spline
 
 
 # Define function for string formatting of scientific notation
